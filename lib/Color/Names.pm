2fc2c454b864be5c962d9eb6f03eaf6d6b38217d
clifton.wood@gmail.com <clifton.wood@gmail.com>
2016-08-27 23:55:30 -0400

    - Add public interface. Fixes issue with programmatic variables.

---
+++ lib/Color/Names.pm
@@ -0,0 +1,48 @@
+use v6.c;
+
+unit package Color::Names;
+
+# cw: $*REPO.repo-chain list of CompUnit::Repository::Installation objects
+#     that contain path info.
+
+my @color_lists;
+
+BEGIN {
+	for @($*REPO.repo-chain).grep({
+		$_ ~~ CompUnit::Repository::FileSystem
+		||
+		$_ ~~ CompUnit::Repository::Installation
+	}) -> $c {
+		my $p = $c.path-spec.subst(/^ .+ '#'/, '');
+		if "{$p}/Color/Names".IO.d {
+			for dir("{$p}/Color/Names") -> $f { 
+				my $b = $f.basename;
+				$b ~~ s/ '.' pm6?//;
+				push @color_lists: $b;
+			}
+
+			last;
+		}
+	}
+}
+
+INIT {
+	# cw: Really want a SELECTIVE way to load these, instead of doing
+	#     them all at compile time.
+	#require ::("Color::Names::$_") for @color_lists;
+	require ::("Color::Names::Cloford");
+}
+
+sub color(Str $n) is export {
+	my $retVal = [];
+
+	for (@color_lists) -> $cl {
+		$retVal.push: $_ => ::("Color::Names::{$cl}::%Colors"){$n}
+			if ::("Color::Names::{$cl}::%Colors"){$n}:exists;
+	}
+
+	# cw: Simplify return value if only one match.
+	$retVal = $retVal[0].value if $retVal.elems == 1;
+
+	$retVal;
+}
